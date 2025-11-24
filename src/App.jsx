3fd63061071eeb4f3059ab6e5aca8b2e10e8e773
Natalya Nassimi <natalya1ahronee@gmail.com>
2025-11-24 16:19:51 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main>
+      <Routes>
+        
+      </Routes>
+    </main>
+    </>
+  )
+}
+
+export default App
