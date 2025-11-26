deb56f31b158eb142cc9485d4e9dc9e305388da8
Natalya Nassimi <natalya1ahronee@gmail.com>
2025-11-26 10:41:18 +0000

    recipe details classes

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { recipeShow } from "../../services/recipes";
 import { useParams } from "react-router";
+import './RecipeDetails.css'
+
 const RecipeDetails = () => {
     const [recipe, setRecipe] = useState({})
     const [loading, setLoading] = useState(true);
@@ -17,32 +19,40 @@ const RecipeDetails = () => {
         }
         getRecipe();
     }, [])
+
     return (
-        loading ? <p>Loading ...</p> :
+        loading ? <p className="loading">Loading ...</p> :
             <>
-                <section className="titleCard">
-                    <h1>{recipe.name}</h1>
-                    <p>{recipe.author?.username}</p>
-                    <p>{recipe?.createdAt.split("T")[0]}</p>
-                    <p>{recipe.preparationTime}</p>
-                </section>
-                <section >
-                    {recipe.ingredients.map(ingredient=>{
-                        return(
-                        <div>
-                            <p>{ingredient.measurement +` ${ingredient.measurement>1?ingredient.unit+`s`: ingredient.unit}` + ` of ${ingredient.name}`} </p>
-                        </div>
-                        )
-                    })}
-                </section>
-                <section className="instrutions">
-                    <h2>Instructions</h2>
-                    <ol>
-                        {recipe.instructions.map((instruction, index)=>{
-                            return <li key={index}>{instruction}</li>
-                        })}
-                    </ol>
-                </section>
+                <div className='recipe-details-container'>
+                    <section className="recipe-header-card">
+                        <h1>{recipe.name}</h1>
+                        <p className='author'> {recipe.author?.username}</p>
+                        <p className='date'> {recipe?.createdAt.split("T")[0]}</p>
+                        <p className='prep-time'> {recipe.preparationTime}</p>
+                    </section>
+
+                    <div className='details-grid'>
+                        <section className='ingredients'>
+                            <h2>Ingredients</h2>
+                            {recipe.ingredients.map(ingredient => {
+                                return (
+                                    <div>
+                                        <p>{ingredient.measurement + ` ${ingredient.measurement > 1 ? ingredient.unit + `s` : ingredient.unit}` + ` of ${ingredient.name}`} </p>
+                                    </div>
+                                )
+                            })}
+                        </section>
+
+                        <section className="instrutions-card">
+                            <h2>Instructions</h2>
+                            <ol>
+                                {recipe.instructions.map((instruction, index) => {
+                                    return <li key={index}>{instruction}</li>
+                                })}
+                            </ol>
+                        </section>
+                    </div>
+                </div>
             </>
     )
 }
